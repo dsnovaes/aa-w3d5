8cf42cde62fa78db620c617ea581f139fcf9669a
Diego Novaes <dsnovaes1@gmail.com>
2022-10-21 12:20:02 -0700

    first commit

---
+++ lib/00_tree_node.rb
@@ -0,0 +1,42 @@
+class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent)
+        @parent = parent
+        if self.parent != nil
+            old_parent = self.parent.children 
+
+            # if the node is already child of other node
+            idx = 0
+            if old_parent.empty?
+                old_parent.each_with_index do |ele,i|
+                    if ele == self
+                        idx = i
+                        break
+                    end
+                end
+                old_parent.delete_at(idx)
+            end
+        end
+
+        if self.parent != nil && !parent.children.include?(self)
+            parent.children << self 
+        end
+    end
+
+end
+
+# root = PolyTreeNode.new("a")
+# child = PolyTreeNode.new("b")
+
+# child.parent = root
+# p child
+# puts
+# p root
